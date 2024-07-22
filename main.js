ccaf91c24895c7e2d8cfdeb24ff16b1d6066204d
Meet Thakur <99238677+MeetThakur@users.noreply.github.com>
2024-07-22 15:16:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+gsap.registerPlugin(ScrollTrigger) 
+
 var line = document.querySelector('.line');
 var width = line.offsetWidth;
 var ipath = `M 0 50 Q ${width/2} 50 ${width} 50`;
@@ -19,3 +22,207 @@ line.addEventListener('mouseleave', function(e) {
         duration: 1,
     })
 });
+
+
+var tl  = gsap.timeline();
+
+tl.to(".loader",{
+	delay: 3,
+	opacity: 0,
+	display: "none",
+});
+
+tl.to("html",{
+	overflowY:"scroll",
+})
+
+tl.from('nav',{
+    opacity: 0,
+    duration: 0.8,
+})
+
+tl.from('nav a',{
+    y: -5,
+    opacity: 0,
+    duration: 0,
+})
+
+tl.from('.tagline',{
+    y: -50,
+    opacity: 0,
+    duration: 0.8,
+}
+)
+
+tl.from('.estb',{
+    x: -50,
+    opacity: 0,
+    duration: 0.8,
+})
+
+
+tl.from('.bgText',{
+    z: -50,
+    opacity: 0,
+    duration: 0.8,
+})
+
+
+gsap.from('#story .title', {
+    scrollTrigger: {
+        trigger: '#story .title',
+        start: 'top 60%',
+        end: 'top 20%',
+        scrub:2,
+    },
+    opacity:0,
+    y:-100,
+})
+
+
+gsap.from('#story p', {
+    scrollTrigger: {
+        trigger: '#story .title',
+        start: 'top 60%',
+        end: 'top 20%',
+        scrub:2,
+    },
+    opacity:0,
+    y:-100,
+})
+
+
+gsap.from('.strbg', {
+    scrollTrigger: {
+        trigger: '#story .title',
+        start: 'top 20%',
+        end: 'top 0%',
+        scrub:2,
+        duration: 3,
+    },
+    opacity:0,
+})
+
+
+gsap.from('#menu .title', {
+    scrollTrigger: {
+        trigger: '#menu',
+        start: 'top 60%',
+        end: 'top 30%',
+        scrub:2,
+        markers: true,
+    },
+    opacity:0,
+    y:-100,
+})
+
+gsap.from('#menu #one', {
+    scrollTrigger: {
+        trigger: '#menu',
+        start: 'top 40%',
+        end: 'top 30%',
+        scrub:2,
+        markers: true,
+    },
+    opacity:0,
+    x:-200,
+})
+
+gsap.from('#menu #two', {
+    scrollTrigger: {
+        trigger: '#menu',
+        start: 'top 40%',
+        end: 'top 30%',
+        scrub:2,
+        markers: true,
+    },
+    opacity:0,
+    x:200,
+
+})
+
+gsap.from('.m1', {
+    scrollTrigger: {
+        trigger: '#menu',
+        start: 'top 40%',
+        end: 'top 30%',
+        scrub:2,
+        markers: true,
+    },
+    opacity:0,
+    y:200,
+})
+
+
+gsap.from('.m2', {
+    scrollTrigger: {
+        trigger: '#menu',
+        start: 'top 40%',
+        end: 'top 30%',
+        scrub:2,
+        markers: true,
+    },
+    opacity:0,
+    x:-250,
+})
+
+gsap.from('#blog .title', {
+    scrollTrigger: {
+        trigger: '#blog',
+        start: 'top 60%',
+        end: 'top 20%',
+        scrub:2,
+        markers: true,
+    },
+    opacity:0,
+    y:-100,
+})
+
+
+gsap.from('.cards', {
+    scrollTrigger: {
+        trigger: '.cards',
+        start: 'top 60%',
+        end: 'top 30%',
+        scrub:2,
+        markers: true,
+    },
+    opacity:0,
+    scale:0.8,
+})    
+
+gsap.from('#book .title', {
+    scrollTrigger: {
+        trigger: '#book',
+        start: 'top 60%',
+        end: 'top 20%',
+        scrub:2,
+        markers: true,
+    },
+    opacity:0,
+    y:-100,
+})
+
+
+gsap.from('#book form', {
+    scrollTrigger: {
+        trigger: '#book',
+        start: 'top 60%',
+        end: 'top 20%',
+        scrub:2,
+        markers: true,
+    },
+    opacity:0,
+    scale:0.8,
+})
+
+
+gsap.from('.bookbg', {
+    scrollTrigger: {
+        trigger: '#book',
+        start: 'top 20%',
+        end: 'top 0%',
+        scrub:2,
+    },
+    opacity:0,
+})
